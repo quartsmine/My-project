0efa326e04e2f1a8fde771e0dcac4de7eb88248e
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-11-23 21:17:03 -0500

    1123_In class, trigger.

---
+++ Assets/Daniel/script/Trigger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject doorHinge;
+    public Vector3 openDoorDegrees;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        OpenDoor();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("untriggered");
+
+    }
+
+
+    public void OpenDoor() 
+    {
+        doorHinge.GetComponent<Transform>().Rotate(openDoorDegrees, Space.World);
+    }
+
+
+}
